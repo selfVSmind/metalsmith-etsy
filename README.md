424223e45fdc81743bf0e1e63ae3265629b6ec1f
Jason <pitbullent@gmail.com>
2016-09-06 17:29:54 +0000

    Early Stages Still

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # metalsmith-etsy
-A small plugin for Metalsmith to incorporate your Etsy store into your website.
+A small plugin for Metalsmith to incorporate your Etsy store into your static website.
+
+## Installation
+    $ npm install --save metalsmith-etsy
+
+## Example Build File
+```js
+var Metalsmith = require('metalsmith'),
+    markdown = require('metalsmith-markdown'),
+    templates = require('metalsmith-templates'),
+    collections = require('metalsmith-collections'),
+    permalinks = require('metalsmith-permalinks'),
+    etsy = require('metalsmith-etsy');
+
+Metalsmith(__dirname)
+    .use(etsy({
+        api_key: <your_etsy_api_key>,
+        etsy_shop  : 'StickToThePlannerCOM'
+    }))
+    .use(collections({
+        pages: {
+            pattern: 'content/pages/*.md'
+        },
+        articles: {
+            pattern: 'content/articles/*.md',
+            sortBy: 'date'
+        },
+        listings: {
+            pattern: 'listings/*.md'
+        }
+    }))
+    .use(markdown())
+    .use(permalinks({
+        pattern: ':collections/:title'
+    }))
+    .use(templates({
+        engine: 'handlebars',
+        partials: {
+            header: 'partials/header',
+            footer: 'partials/footer'
+        }
+    }))
+    .destination('./build')
+    .build(function (err) { if(err) console.log(err) })
+```
+
+## Better Documentation Coming
+For now read about this [on my blog][http://jasonlambert.io/articles/metalsmith-etsy/]
--- index.js
+++ index.js
@@ -1,8 +1,61 @@
-// this package does NOTHING yet..
-// it's only a placeholder for now
+var getJSON = require('get-json')
+var request = require('sync-request');
 
-module.exports = {
-    placeholder: function(output) {
-        console.log(output);
+module.exports = function (options) {
+    
+    var my_etsy_api_key = options.api_key
+    var my_etsy_shop_name = options.etsy_shop
+
+    return function (files, metalsmith, done) {
+
+        // you have to create a new etsy app here: 
+        getJSON('https://openapi.etsy.com/v2/shops/' + my_etsy_shop_name + '/listings/active?limit=5&api_key='+my_etsy_api_key, function(error, response){
+         
+            if(error) console.log(error)
+
+            else {
+                
+                var allActiveListings = []
+                
+                // loop through every active etsy listing in the shop
+                response.results.forEach(function(listing) {
+                    
+                    // make up a name of a virtual markdown file named after the etsy listing id
+                    var workingFile = 'listings/' + listing.listing_id + '.md'
+                    
+                    // push that file to allActiveListings
+                    allActiveListings.push('listings/' + listing.listing_id + '.html')
+                    
+                    // OK this is the fun part!
+                    // We're creating a virtual markdown file that the other Metalsmith plugins can manipulate!!
+                    // When doing this sort of thing DO NOT FORGET the contents variable.. It is required by the Metalsmith environment
+                    // Also note that we're using a Handlebars template named listing that we will define next
+                    files[workingFile] = {title: listing.title, contents: listing.description, etsy_link: listing.url, template: 'listing.hbt'}
+
+                    // let's grab all of the images for the listing
+                    // etsy api requires that you do this in a seperate request
+                    var res = request('GET', 'https://openapi.etsy.com/v2/listings/' + listing.listing_id + '/images?api_key='+my_etsy_api_key);
+
+                    var jsonRes = JSON.parse(res.getBody())
+                    
+                    // I'm choosing to make an array with a url to each image
+                    // We're going to pass this into the virtual markdown file as YAML data
+                    files[workingFile].listingImages = []
+                    jsonRes.results.forEach(function(result) {
+                        files[workingFile].listingImages.push(result.url_fullxfull)
+                    })
+
+                    // OK this last part is so we can reference our virtual files later from the site's root index.md
+                    var indexContentString = ''
+                    allActiveListings.forEach(function(listingLink) {
+                        indexContentString += '<a href="' + listingLink + '">' + listingLink + '</a><br>'
+                    })
+                    files['index.md'].contents = indexContentString
+                })
+            }
+            
+            // done() signifies our re-entry into the daisychain we snuck into earlier
+            done()         
+        })
     }
-};
+}
